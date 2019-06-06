8cc6c336d51b8c87fde330e8dd315a83067e9c72
maguirre1 <magu@stanford.edu>
2019-06-05 23:16:44 -0700

    beautification

--- scripts/variant_qc.sh
+++ scripts/variant_qc.sh
@@ -5,6 +5,10 @@ zcat /oak/stanford/groups/mrivas/private_data/ukbb/variant_filtering/variant_fil
 
 # MAF > 0.01%, missingness < 5%, all filters for array sites, LD independent
 plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/array_combined/pgen/ukb24983_cal_hla_cnv \
-       --maf 1e-4 --geno 0.05 --exclude not_all_filters.txt --indep-pairwise 50 5 0.5 --memory 30000 \
        --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
+       --exclude not_all_filters.txt \
+       --indep-pairwise 50 5 0.5 \
+       --maf 1e-4 \
+       --geno 0.05 \
+       --memory 30000 \
        --out ../reference/variant_qc 
