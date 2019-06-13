4e41e1810cfbec0164616e03ffac7882cfa6f9d8
Matthew Aguirre <magu@sh-109-55.int>
2019-06-13 16:32:49 -0700

    features added

--- scripts/PRS.py
+++ scripts/PRS.py
@@ -1,8 +1,17 @@
 #!/bin/python
 import sys
 import os
+import errno
 import numpy as np
 import pandas as pd
+
+_README_="""
+A script to compute vanilla (threshold and clump) from a dataset that would be
+input to DEGAS (tsvd.py). No clumping is necessary since the input file only
+contains variants which are in LE with one another (pruned).
+
+Author: Matthew Aguirre (SUNET: magu)
+"""
 
 # ensure usage
 if len(sys.argv) < 3:
@@ -14,9 +23,15 @@ dataset = sys.argv[1]
 pheno = sys.argv[2]
 
 # work here
-out_dir = os.path.join(os.path.dirname(dataset), 'PRS')
-if not os.path.exists(out_dir):
-    os.mkdir(out_dir)
+splitext = lambda f: os.path.splitext(f)[0] # convenient
+out_dir = splitext(splitext(splitext(dataset))).replace('datasets','PRS')
+try:
+    os.makedirs(out_dir)
+except OSError as exc:  # Python >2.5
+    if exc.errno == errno.EEXIST and os.path.isdir(out_dir):
+        pass
+    else:
+        raise
 
 # load data
 bfile="/oak/stanford/groups/mrivas/ukbb24983/array_combined/pgen/ukb24983_cal_hla_cnv"
@@ -29,7 +44,10 @@ weight_file = os.path.join(out_dir, pheno+'_weights.txt')
 coefs[['ALT',pheno]].dropna().to_dense().to_csv(weight_file, sep='\t', header=None)
 
 # score alleles with plink
+m_phe="/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/master_phe/master.phe"
 os.system("ml load biology; ml load plink")
 os.system(" ".join(["plink", "--bfile", bfile,
                              "--score", weight_file, "sum center double-dosage",
+                             "--pheno", m_phe, "--pheno-name", pheno,
+                             "--memory 8000",
                              "--out", os.path.join(out_dir, pheno+"_PRS")]))
