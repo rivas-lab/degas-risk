c5f09d053f0d3de707783a83cb4dc997301a288b
Matthew Aguirre <magu@sh-ln03.stanford.edu>
2019-07-18 15:45:31 -0700

    added more stats to validation

--- validation/validate.py
+++ validation/validate.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import sys,os
+from statsmodels.formula.api import ols,Logit
 
 # ensure usage, process input
 if len(sys.argv) < 3:
@@ -27,21 +28,35 @@ nbw=pd.read_table('/oak/stanford/groups/mrivas/ukbb24983/sqc/population_'+
                    header=None).iloc[:,0].tolist()
 phenos=pd.read_table('/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/'+
                      'master_phe/master.phe', 
-                     na_values=-9, usecols=['IID']+phe, index_col='IID')
+                     usecols=['IID','age','sex','PC1','PC2','PC3','PC4']+phe,
+                     index_col='IID', na_values=-9)
 
 # combine data and redefine cohorts 
 data=pd.merge(phenos, pcs, left_index=True, right_index=True)
 wbr=list(filter(lambda i: i in data.index, wbr))
 nbw=list(filter(lambda i: i in data.index, nbw))
 
-# compute validation for all phenotypes
+# set up output file, initialize scores, metrics, and null model formula
 out=os.path.join(os.path.dirname(sys.argv[2]), 'results', 
-                os.path.splitext(os.path.basename(sys.argv[2]))[0]+'.pearsonr.tsv')
+           os.path.splitext(os.path.basename(sys.argv[2]))[0]+'.pearsonr.tsv')
 data['SCORE']=0
+cols=[x+'_'+y for x in ['WBR','NBW'] for y in ['RAW','RESID','JOINT','COVAR']]
+covs='age+sex+PC1+PC2+PC3+PC4'
+
+# compute validation for all phenotypes
 with open(out, 'w') as o:
+    o.write('\t'.join(['PHE']+cols)+'\n')
     for phe_id in phe:
+        o.write(phe_id+'\t')
+        model=ols if 'INI' in phe_id or 'QT' in phe_id else Logit
         weights=npz['V'][np.where(npz['label_phe_code'] == phe_id),:].flatten()
         data['SCORE']=data.iloc[:,-501:-1].dot(weights)
-        r1=data.loc[wbr,['SCORE',phe_id]].corr(method='pearson').iloc[0,1]
-        r2=data.loc[nbw,['SCORE',phe_id]].corr(method='pearson').iloc[0,1]
-        o.write('\t'.join([phe_id,str(r1),str(r2)])+'\n')
+        for pop in [wbr,nbw]:
+            r1=data.loc[pop,['SCORE',phe_id]].corr().iloc[0,1]
+            nm=model(formula=phe_id+'~'+covs, data=data.loc[pop]).fit()
+            r2=ols(nm.pearson_resid, data.loc[pop,'SCORE']).fit().rsquared_adj
+            r3=model(formula=phe_id+'~'+covs+'+SCORE', 
+                        data=data.loc[pop]).fit().rsquared_adj
+            r4=nm.rsquared_adj
+            o.write('\t'.join(map(str,[r1,r2,r3,r4])))
+        o.write('\n')
