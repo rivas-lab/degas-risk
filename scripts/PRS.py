2d5a5652bc901ad5eef7a6ccd7fba22d1aca2219
Matthew Aguirre <magu@sh-102-07.int>
2019-09-15 01:20:24 -0700

    testing phenotype-level analysis pipeline

--- scripts/PRS.py
+++ scripts/PRS.py
@@ -13,41 +13,44 @@ contains variants which are in LE with one another (pruned).
 Author: Matthew Aguirre (SUNET: magu)
 """
 
-# ensure usage
-if len(sys.argv) < 3:
-    print("usage: python " + sys.argv[0] + " dataset.pkl.gz PHE_ID")
-    sys.exit(1)
+# function used in pipeline
+def PRS(dataset, pheno):
+    # work here
+    splitext = lambda f: os.path.splitext(f)[0] # convenient
+    out_dir = splitext(splitext(splitext(dataset))).replace('datasets','PRS')
+    try:
+        os.makedirs(out_dir)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(out_dir):
+            pass
+        else:
+            raise
 
-# input
-dataset = sys.argv[1]
-pheno = sys.argv[2]
+    # load data
+    bfile="/oak/stanford/groups/mrivas/ukbb24983/array_combined/pgen/ukb24983_cal_hla_cnv"
+    bim = pd.read_table(bfile+'.pvar', index_col='ID')
+    coefs = pd.read_pickle(dataset)
+    coefs = coefs.join(bim['ALT'])
 
-# work here
-splitext = lambda f: os.path.splitext(f)[0] # convenient
-out_dir = splitext(splitext(splitext(dataset))).replace('datasets','PRS')
-try:
-    os.makedirs(out_dir)
-except OSError as exc:  # Python >2.5
-    if exc.errno == errno.EEXIST and os.path.isdir(out_dir):
-        pass
-    else:
-        raise
+    # extract coefficients
+    weight_file = os.path.join(out_dir, pheno+'_weights.txt')
+    coefs[['ALT',pheno]].dropna().to_dense().to_csv(weight_file, sep='\t', header=None)
 
-# load data
-bfile="/oak/stanford/groups/mrivas/ukbb24983/array_combined/pgen/ukb24983_cal_hla_cnv"
-bim = pd.read_table(bfile+'.pvar', index_col='ID')
-coefs = pd.read_pickle(dataset)
-coefs = coefs.join(bim['ALT'])
+    # score alleles with plink
+    m_phe="/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/master_phe/master.phe"
+    os.system("ml load biology; ml load plink")
+    os.system(" ".join(["plink", "--bfile", bfile,
+                                 "--score", weight_file, "sum center double-dosage",
+                                 "--pheno", m_phe, "--pheno-name", pheno,
+                                 "--memory 8000",
+                                 "--out", os.path.join(out_dir, pheno+"_PRS")]))
 
-# extract coefficients
-weight_file = os.path.join(out_dir, pheno+'_weights.txt')
-coefs[['ALT',pheno]].dropna().to_dense().to_csv(weight_file, sep='\t', header=None)
-
-# score alleles with plink
-m_phe="/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/master_phe/master.phe"
-os.system("ml load biology; ml load plink")
-os.system(" ".join(["plink", "--bfile", bfile,
-                             "--score", weight_file, "sum center double-dosage",
-                             "--pheno", m_phe, "--pheno-name", pheno,
-                             "--memory 8000",
-                             "--out", os.path.join(out_dir, pheno+"_PRS")]))
+# 
+if __name__ == '__main__':
+    # ensure usage
+    if len(sys.argv) < 3:
+        print("usage: python " + sys.argv[0] + " dataset.pkl.gz PHE_ID1 ...")
+        sys.exit(1)
+    # do the thing
+    for phe in sys.argv[2:]:
+        PRS(sys.argv[1], phe)
