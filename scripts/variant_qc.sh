f896d3bf2881632d061d44b74b5ff35ee1fceeb0
maguirre1 <magu@stanford.edu>
2021-02-17 11:38:27 -0800

    redacted part 3

--- scripts/variant_qc.sh
+++ scripts/variant_qc.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
 # get "all" filters (PTV paper)
-zcat /oak/stanford/groups/mrivas/private_data/ukbb/variant_filtering/variant_filter_table.tsv.gz | awk -F'\t' '($30 != 0){print $5}' > not_all_filters.txt
+ukbb_dir=""
+ukb2_dir=""
+zcat ${ukbb_dir}/variant_filtering/variant_filter_table.tsv.gz | awk -F'\t' '($30 != 0){print $5}' > not_all_filters.txt
 
 
 # 1a. array-specific LD pruning and missingness (Axiom)
-plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb24983_cal_cALL_v2 \
-       --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
-       --keep-fam /oak/stanford/groups/mrivas/ukbb24983/sqc/axiom_individuals.txt \
+plink2 --pfile ${ukb2_dir}/cal/pgen/ukb24983_cal_cALL_v2 \
+       --keep ${ukb2_dir}/sqc/population_stratification/ukb24983_white_british.phe \
+       --keep-fam ${ukb2_dir}/sqc/axiom_individuals.txt \
        --exclude not_all_filters.txt \
-       --extract /oak/stanford/groups/mrivas/ukbb24983/sqc/axiom_specific_variants.txt \
+       --extract ${ukb2_dir}/sqc/axiom_specific_variants.txt \
        --maf 1e-4 --geno 0.05 \
        --indep-pairwise 50 5 0.5 \
        --out ../reference/temp_axiom
 # keep-fam is equivalent to keep, and is used because there is no --keep-intersect
 
 # 1b. array-specific LD pruning and missingness (BiLEVE)
-plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb24983_cal_cALL_v2 \
-       --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
-       --keep-fam /oak/stanford/groups/mrivas/ukbb24983/sqc/bileve_individuals.txt \
+plink2 --pfile ${ukb2_dir}/cal/pgen/ukb24983_cal_cALL_v2 \
+       --keep ${ukb2_dir}/sqc/population_stratification/ukb24983_white_british.phe \
+       --keep-fam ${ukb2_dir}/sqc/bileve_individuals.txt \
        --exclude not_all_filters.txt \
-       --extract /oak/stanford/groups/mrivas/ukbb24983/sqc/bileve_specific_variants.txt \
+       --extract${ukb2_dir}/sqc/bileve_specific_variants.txt \
        --maf 1e-4 --geno 0.05 \
        --indep-pairwise 50 5 0.5 \
        --out ../reference/temp_bileve
 
 # 1c. array-specific LD pruning and missingness (CNV)
-plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/cnv/pgen/cnv \
-       --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
+plink2 --pfile ${ukb2_dir}/cnv/pgen/cnv \
+       --keep ${ukb2_dir}/population_stratification/ukb24983_white_british.phe \
        --maf 1e-4 --geno 0.05 \
        --indep-pairwise 50 5 0.5 \
        --out ../reference/temp_cnv
 
 # 1d. array-specific LD pruning and missingness (HLA)
-plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/hla/pgen/ukb_hla_v3 \
-       --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
+plink2 --pfile ${ukb2_dir}/hla/pgen/ukb_hla_v3 \
+       --keep ${ukb2_dir}/sqc/population_stratification/ukb24983_white_british.phe \
        --maf 1e-4 --geno 0.05 \
        --indep-pairwise 50 5 0.5 \
        --out ../reference/temp_hla
