c61cecef3d5ade47e66e395f954f6d8987c74d85
maguirre1 <magu@stanford.edu>
2021-02-17 11:40:27 -0800

    redacted part 3b

--- scripts/variant_qc.sh
+++ scripts/variant_qc.sh
@@ -47,10 +47,10 @@ cat ../reference/temp_*.prune.in > ../reference/array_specific_filters.txt
 
 
 # 2. missingness and AF for variants on both arrays
-plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb24983_cal_cALL_v2 \
-       --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
+plink2 --pfile ${ukb2_dir}/cal/pgen/ukb24983_cal_cALL_v2 \
+       --keep ${ukb2_dir}/sqc/population_stratification/ukb24983_white_british.phe \
        --exclude not_all_filters.txt \
-       --extract /oak/stanford/groups/mrivas/ukbb24983/sqc/both_array_variants.txt \
+       --extract ${ukb2_dir}/sqc/both_array_variants.txt \
        --maf 1e-4 --geno 0.05 --write-snplist \
        --out ../reference/shared_filter
 
@@ -58,8 +58,8 @@ plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb24983_cal_cALL_
 
 # 3. Combine array/cnv/hla-specific filters, and missingness from shared variants
 #     then add a more aggressitve MAF cutoff and re-do LD pruning 
-plink2 --pfile /oak/stanford/groups/mrivas/ukbb24983/array_combined/pgen/ukb24983_cal_hla_cnv \
-       --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
+plink2 --pfile ${ukb2_dir}/array_combined/pgen/ukb24983_cal_hla_cnv \
+       --keep ${ukb2_dir}/sqc/population_stratification/ukb24983_white_british.phe \
        --exclude not_all_filters.txt \
        --extract ../reference/array_specific_filters.txt ../reference/shared_filter.snplist \
        --indep-pairwise 50 5 0.5 \
